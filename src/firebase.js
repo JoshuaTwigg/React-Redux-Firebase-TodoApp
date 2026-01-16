ebb390d56eda21071c827731f7440ed5f786c7e9
Josh <105489684+JoshuaTwigg@users.noreply.github.com>
2026-01-16 14:31:27 +0200

    Add files via upload
    hide data

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBTNqy1dmL57MOcTR4HkYem8oxE42v4anQ",
-  authDomain: "todoapp-f5a25.firebaseapp.com",
-  projectId: "todoapp-f5a25",
-  storageBucket: "todoapp-f5a25.firebasestorage.app",
-  messagingSenderId: "929909687186",
-  appId: "1:929909687186:web:9fc3537952d24ebb79374b",
-  measurementId: "G-T5WFREBN0P"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
